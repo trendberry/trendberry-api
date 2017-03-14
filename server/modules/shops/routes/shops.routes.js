93a6a5bb780f1db1bdaddb1eefa10f634a48264f
Den1258 <dn.den1@gmail.com>
2017-03-14 19:47:33 +0200

    -

--- server/modules/shops/routes/shops.routes.js
+++ server/modules/shops/routes/shops.routes.js
@@ -5,11 +5,11 @@ var shopsPolicy = require('../policies/shops.policy'),
 
 module.exports = function(app) {
   // Shops Routes
-  app.route('/api/shops').all(shopsPolicy.isAllowed)
+  app.route('/api/shops')//.all(shopsPolicy.isAllowed)
     .get(shops.list)
     .post(shops.create);
 
-  app.route('/api/shops/:shopId').all(shopsPolicy.isAllowed)
+  app.route('/api/shops/:shopId')//.all(shopsPolicy.isAllowed)
     .get(shops.read)
     .put(shops.update)
     .delete(shops.delete);
