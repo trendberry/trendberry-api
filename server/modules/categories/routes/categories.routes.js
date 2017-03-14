4a365f7f0a648a76f492de97f84d47af43502f78
Den1258 <dn.den1@gmail.com>
2017-03-14 19:46:19 +0200

    -

--- server/modules/categories/routes/categories.routes.js
+++ server/modules/categories/routes/categories.routes.js
@@ -8,23 +8,23 @@ var categoriesPolicy = require('../policies/categories.policy'),
 
 module.exports = function (app) {
   // Categories collection routes
-  app.route('/api/categories').all(categoriesPolicy.isAllowed)
+  app.route('/api/categories')//.all(categoriesPolicy.isAllowed)
   
     .get(categories.list)
     .post(categories.create);
 
   // Single category routes
-  app.route('/api/categories/:categoryId').all(categoriesPolicy.isAllowed)
+  app.route('/api/categories/:categoryId')//.all(categoriesPolicy.isAllowed)
     .get(categories.read)
     .put(categories.update)
     .delete(categories.delete);
 
   // Category pictures routes
-  app.route('/api/categories/:categoryId/pictures').all(categoriesPolicy.isAllowed)
+  app.route('/api/categories/:categoryId/pictures')//.all(categoriesPolicy.isAllowed)
     .post(categories.pictureCreate);
 
   // Single category routes
-  app.route('/api/categories/:categoryId/pictures/:pictureId').all(categoriesPolicy.isAllowed)
+  app.route('/api/categories/:categoryId/pictures/:pictureId')//.all(categoriesPolicy.isAllowed)
     .delete(categories.pictureDelete);
 
   // Finish by binding the Category middleware
